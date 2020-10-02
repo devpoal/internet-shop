4527a534b5debd6365e5a1e427852ebc94add7e8
Aleksandr Popovichenko <devpoal@gmail.com>
2020-10-02 10:57:56 +0300

    Updates

---
+++ precache-manifest.ff49a141d2ead313564651a9b93fe955.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a42402ac87ba2625be2561db28b6061",
+    "url": "/internet-shop/index.html"
+  },
+  {
+    "revision": "92735e141a78682be458",
+    "url": "/internet-shop/static/css/main.eea1d7ed.chunk.css"
+  },
+  {
+    "revision": "e88b64c0d54a1cc7cec7",
+    "url": "/internet-shop/static/js/2.003952cc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/internet-shop/static/js/2.003952cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92735e141a78682be458",
+    "url": "/internet-shop/static/js/main.fa206280.chunk.js"
+  },
+  {
+    "revision": "244f6af46f0c5f7dde6f",
+    "url": "/internet-shop/static/js/runtime-main.05c5a642.js"
+  }
+]);
